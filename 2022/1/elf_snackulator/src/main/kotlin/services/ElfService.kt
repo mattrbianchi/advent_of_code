816ec0aa34885e2589dab2b8e1b6f08d6bdf24d0
Matt Bianchi <mbianchi@cedar.com>
2022-12-02 16:04:32 -0500

    Refactors list opertions

--- 2022/1/elf_snackulator/src/main/kotlin/services/ElfService.kt
+++ 2022/1/elf_snackulator/src/main/kotlin/services/ElfService.kt
@@ -4,9 +4,9 @@ import entities.Elf
 import store.AbstractElfRepository
 
 fun elfWithMostCaloricSnacks(repo: AbstractElfRepository): Elf {
-    return repo.list().maxWith(compareBy<Elf>{ it.totalCalories() })
+    return repo.list().maxBy { it.totalCalories() }
 }
 
 fun topThreeElvesWithMostCaloricSnacks(repo: AbstractElfRepository): List<Elf> {
-    return repo.list().sortedWith(compareBy<Elf>{ it.totalCalories() }).asReversed().subList(0, 3)
+    return repo.list().sortedByDescending { it.totalCalories() }.take(3)
 }
